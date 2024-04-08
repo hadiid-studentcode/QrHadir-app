823913bde35fd887f39726838666c07a0a5c22b1
hadiid-studentcode <hadiidandri2000@gmail.com>
2024-04-08 15:28:27 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,28 @@
+
+
+
+
 <?php
+
+
+// use App\Http\Controllers\ProfileController;
+// use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// require __DIR__ . '/auth.php';
 
 use App\Http\Controllers\AbsensiController;
 use App\Http\Controllers\DashboardController;
@@ -31,4 +55,5 @@ Route::resource('/kelola-absensi', KelolaAbsenController::class);
 Route::get('/absensi/{qr_code}', [AbsensiController::class, 'store'])->name('absen.store');
 
 Route::get('/absensi', [AbsensiController::class, 'index'])->name('absen.index');
+
 
